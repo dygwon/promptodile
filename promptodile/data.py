a0acbec48cccd8f54d169867a4a74cfa08af93d8
Daniel Gwon <daniel.gwon@ll.mit.edu>
2025-09-26 16:53:40 -0400

    bugs from using corpus other than BEIR

--- promptodile/data.py
+++ promptodile/data.py
@@ -17,62 +17,63 @@ Corpus: TypeAlias = dict[str, dict[str, str]]
 FewShot: TypeAlias = dict[str, dict[str, int]]
 Queries: TypeAlias = dict[str, str]
 
+
 class Data:
     def __init__(self, config: shared_config.SharedConfig):
         self._config = config
         print(f'Corpus: {self._config.corpus_jsonl}')
         print(f'Queries: {self._config.queries_jsonl}')
         print(f'Few-shot Examples: {self._config.examples_txt}')
-        
+
         self._corpus = None
         self._queries = None
         self._few_shot = None
         self._num_few_shot = 0
-        
+
         if self._config.corpus_jsonl:
             self._corpus = self._load_corpus()
         if self._config.queries_jsonl:
             self._queries = self._load_queries()
         if self._config.examples_txt:
             self._few_shot = self._load_few_shot()
-    
+
     @property
     def num_few_shot(self) -> int:
         return self._num_few_shot
-    
+
     @property
     def corpus(self) -> Corpus | None:
         return self._corpus
-    
+
     @property
     def queries(self) -> Queries | None:
         return self._queries
-    
+
     @property
     def few_shot(self) -> FewShot | None:
         return self._few_shot
-    
+
     def _load_corpus(self) -> Corpus | None:
         print('Loading corpus')
-        
+
         corpus: Corpus = {}
         corpus_jsonl = self._config.corpus_jsonl
         if corpus_jsonl is None:
             return
         with open(corpus_jsonl, mode='r', encoding='utf-8') as fin:
             for line in fin:
-                line_dict = json.loads(line)
+                line_dict: dict[str, str] = json.loads(line)
                 corpus[line_dict['docid']] = {
-                    'title': line_dict['title'],
-                    'body': line_dict['body']
+                    'title': line_dict.get('title', ''),
+                    'body': line_dict['body'],
                 }
         print(f'Loaded {len(corpus)} documents.')
-                
+
         return corpus
-    
+
     def _load_queries(self) -> Queries | None:
         print('Loading queries...')
-        
+
         queries: Queries = {}
         queries_jsonl = self._config.queries_jsonl
         if queries_jsonl is None:
@@ -80,14 +81,15 @@ class Data:
         with open(queries_jsonl, mode='r', encoding='utf-8') as fin:
             for line in fin:
                 line_dict = json.loads(line)
-                queries[line_dict['id']] = line_dict['narrative']
+                qid = str(line_dict['id'])
+                queries[qid] = line_dict['narrative']
         print(f'Loaded {len(queries)} queries.')
-        
+
         return queries
-    
+
     def _load_few_shot(self) -> FewShot | None:
         print('Loading few-shot examples.')
-        
+
         few_shot: FewShot = {}
         examples_txt = self._config.examples_txt
         if examples_txt is None:
@@ -97,5 +99,5 @@ class Data:
                 qid, _, docid, score = line.split()
                 few_shot.setdefault(qid, {})[docid] = int(score)
                 self._num_few_shot += 1
-        
+
         return few_shot
